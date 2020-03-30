e3571fdd6924e470a4520033fdf575c1d689b576
amiineh <amineh.ahm@gmail.com>
2020-03-30 16:53:29 +0430

    added square and split

--- README.md
+++ README.md
@@ -2,13 +2,14 @@ Using this repository you can pixelize your images into hexagon grid!
 
 ### Samples
 Normal image
-![Normal](samples/sample.jpg)
-Pixelized with Radius = 5
-![R=5](samples/pixelized_5.jpg)
-Pixelized with Radius = 15
-![R=15](samples/pixelized_15.jpg)
-Pixelized with Radius = 30
-![R=30](samples/pixelized_30.jpg)
+
+Hexagonal             | Square          |       Splited
+:-------------------------:|:-------------------------:|:-------------------------:
+![Normal](samples/sample.jpg) | ![Normal](samples/monroe.jpg)  |  ![Normal](samples/sprited.jpg)
+![R=5](samples/pixelized_5.jpg) | ![R=30](samples/monroe_30.jpg)  |  ![R=5](samples/split_5.jpg)
+![R=15](samples/pixelized_15.jpg) | ![R=20](samples/monroe_20.jpg)  |  ![R=50](samples/split_50.jpg)
+![R=30](samples/pixelized_30.jpg) | ![R=10](samples/monroe_10.jpg)  |  ![R=100](samples/split_100.jpg)
+
 
 
 ### Requirements
@@ -22,11 +23,11 @@ pip install -r requirements.txt
 
 ### How to run
 ```bash
-python main.py your_image.jpg output_image.jpg Radius
+python main.py your_image.jpg output_image.jpg mode Radius
 ```
 For example to generate the samples you can run
 ```bash
-python3 main.py samples/sample.jpg samples/pixelized_5.jpg 5
+python3 main.py samples/sample.jpg samples/pixelized_5.jpg hex 5
 ```
 
 Thanks to Stephan Hügel for hexgrid.py code.
--- main.py
+++ main.py
@@ -7,11 +7,8 @@ from hexgrid import calculate_polygons
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
-if __name__ == '__main__':
-    image_name = str(sys.argv[1])
-    outfile_name = str(sys.argv[2])
-    R = int(sys.argv[3])
-    image = Image.open(image_name)
+
+def hexagon(image, R, h):
     image = np.array(image)[:, :, :3]
     rows, cols, _ = image.shape
     hexagon_points = calculate_polygons(0, 0, rows, cols, R)
@@ -54,5 +51,89 @@ if __name__ == '__main__':
             pixelized_image[i, j] = polygon_to_color[polygon]
 
     pixelized_image = Image.fromarray(np.uint8(pixelized_image))
+    return pixelized_image
+
+
+def square(image, R, h):
+    n = R
+    pixel_size = R // 2 if h is None else h
+
+    def get_center(im, w, h, ps=pixel_size):
+        w //= 2
+        h //= 2
+        box = (w - ps // 2, h - ps // 2, w + ps // 2, h + ps // 2)
+        return im.crop(box)
+
+    def get_center_mean(im, w, h, ps=pixel_size):
+        center = get_center(im, w, h, ps)
+        mean_val = np.mean(center, axis=(0, 1))
+        center = np.array(center)
+        center[:, :, :] = mean_val
+        # center.show()
+        return Image.fromarray(center)
+
+    w, h = image.size
+    pixel_size -= (pixel_size % 2)
+    box = (n * pixel_size, n * pixel_size)
+    background = Image.new('RGB', box, (255, 255, 255))
+
+    for i in range(n):
+        for j in range(n):
+            crop_box = (w // n * i, h // n * j, w // n * (i + 1), h // n * (j + 1))
+            cropped = image.crop(crop_box)
+            center = get_center_mean(cropped, w // n, h // n)
+            paste_box = (i * pixel_size, j * pixel_size, (i + 1) * pixel_size, (j + 1) * pixel_size)
+            background.paste(center, paste_box)
+
+    return background
+
+
+def split(image, R, h):
+    n = R
+    h = R//2 if h is None else h
+
+    def crop(im, w1, h1, n):
+        box = (w1, h1, w1 + n, h1 + n)
+        a = im.crop(box)
+        return a
+
+    width, height = image.size
+    box = (width + (width * h) // n, height + (height * h) // n)
+    background = Image.new('RGB', box, color=(255, 255, 255))
+
+    i_c = 0
+    j_c = 0
+    for i in range(0, height, n):
+        for j in range(0, width, n):
+            foreground = crop(image, j, i, n)
+            box = ((n + h) * (j_c), (n + h) * (i_c))
+            background.paste(foreground, box)
+            j_c += 1
+        i_c += 1
+        j_c = 0
+
+    return background
+
+
+switcher = {
+    'hex': hexagon,
+    'sq': square,
+    'split': split,
+}
+
+
+if __name__ == '__main__':
+    image_name = str(sys.argv[1])
+    outfile_name = str(sys.argv[2])
+    mode = str(sys.argv[3])
+    R = int(sys.argv[4])
+    try:
+        h = int(sys.argv[5])
+    except:
+        h = None
+
+    image = Image.open(image_name)
+    pixelized_image = switcher[mode](image, R, h)
+
     pixelized_image.show()
     pixelized_image.save(outfile_name)
